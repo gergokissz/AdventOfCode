8be2cb6c5100fe17883c9612e4afadb4db89852f
gergokissz <16369083+gergokissz@users.noreply.github.com>
2021-12-01 16:05:39 +0100

    Add 2021 sln and Day1 project

---
+++ AdventOfCode2021/Day1/Program.fs
@@ -0,0 +1,26 @@
+﻿open System.IO
+
+let input = 
+    File.ReadAllLines(@"input.txt")
+    |> Seq.map int
+
+let isIncrement (first, second) =
+    second > first
+
+let increments1 = 
+    input
+    |> Seq.pairwise 
+    |> Seq.where isIncrement
+    |> Seq.length
+
+printfn "Increments1: %i" increments1
+
+let increments3 = 
+    input
+    |> Seq.windowed 3
+    |> Seq.map Array.sum
+    |> Seq.pairwise 
+    |> Seq.where isIncrement
+    |> Seq.length
+
+printfn "Increments3: %i" increments3
