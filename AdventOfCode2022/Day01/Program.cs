ad077983f8001b16954129442b827ca09944bad8
gergokissz <16369083+gergokissz@users.noreply.github.com>
2022-12-01 21:38:57 +0100

    Add 2022 Day01

---
+++ AdventOfCode2022/Day01/Program.cs
@@ -0,0 +1,58 @@
+﻿const string input = "Input.txt";
+
+int Part1()
+{
+    var max = 0;
+    var cal = 0;
+
+    foreach (var line in File.ReadLines(input))
+    {
+        if (string.IsNullOrEmpty(line))
+        {
+            if (cal > max)
+            {
+                max = cal;
+            }
+
+            cal = 0;
+
+            continue;
+        }
+
+        cal += int.Parse(line);
+    }
+
+    return max;
+}
+
+int Part2()
+{
+    var queue = new PriorityQueue<int, int>(3);
+    var cal = 0;
+
+    foreach (var line in File.ReadLines(input))
+    {
+        if (string.IsNullOrEmpty(line))
+        {
+            if (queue.Count < 3)
+            {
+                queue.Enqueue(cal, cal);
+            }
+            else if (cal > queue.Peek())
+            {
+                queue.EnqueueDequeue(cal, cal);
+            }
+
+            cal = 0;
+
+            continue;
+        }
+
+        cal += int.Parse(line);
+    }
+
+    return queue.UnorderedItems.Sum(x => x.Element);
+}
+
+Console.WriteLine(Part1());
+Console.WriteLine(Part2());
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # AdventOfCode
 
-My solutions of the [Advent of Code 2021](https://adventofcode.com/2021) puzzles to learn the basics of F# :)
+My solutions of the [Advent of Code](https://adventofcode.com) puzzles
+
+| Year | Folder | Language |
+| ---- | ------ | -------- |
+| [2021](https://adventofcode.com/2021) | [AdventOfCode2021](./AdventOfCode2021) | F# (just to learn the basics) |
+| [2022](https://adventofcode.com/2022) | [AdventOfCode2022](./AdventOfCode2022) | C# |
+||||
