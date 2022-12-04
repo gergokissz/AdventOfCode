1d9fd8ddce64eff5b9a5e7c59520838ecab5fab0
gergokissz <16369083+gergokissz@users.noreply.github.com>
2022-12-04 09:43:11 +0100

    Add 2022 Day03/Day04

---
+++ AdventOfCode2022/Day03/Program.cs
@@ -0,0 +1,59 @@
+﻿const string input = "Input.txt";
+
+int Part1() =>
+    File.ReadLines(input)
+        .Select(SplitInHalf)
+        .Sum(x => GetPriority(FindCommonChar(x.Item1, x.Item2, string.Empty)));
+
+int Part2() => 
+    File.ReadLines(input)
+        .Batch(3)
+        .Sum(x => GetPriority(FindCommonChar(x[0], x[1], x[2])));
+
+(string, string) SplitInHalf(string text) =>
+    (text[..(text.Length / 2)], text[(text.Length / 2)..text.Length]);
+
+char FindCommonChar(string text1, string text2, string text3)
+{
+    for (var i = 0; i < text1.Length; i++)
+    {
+        // Ignore text3 if it is empty
+        if (text2.Contains(text1[i]) && (text3.Length == 0 || text3.Contains(text1[i])))
+        {
+            return text1[i];
+        }
+    }
+
+    throw new Exception();
+}
+
+int GetPriority(char c) => 
+    c >= 'a' ? c - 'a' + 1 : c - 'A' + 27;
+
+Console.WriteLine(Part1()); // 8153
+Console.WriteLine(Part2()); // 2342
+
+public static class EnumerableExtensions
+{
+    public static IEnumerable<List<T>> Batch<T>(this IEnumerable<T> items, int batchSize)
+    {
+        var batch = new List<T>();
+
+        foreach (var item in items)
+        {
+            batch.Add(item);
+
+            if (batch.Count == batchSize)
+            {
+                yield return batch;
+
+                batch.Clear();
+            }
+        }
+
+        if (batch.Count > 0)
+        {
+            yield return batch;
+        }
+    }
+}
---
+++ AdventOfCode2022/Day04/Program.cs
@@ -0,0 +1,48 @@
+﻿const string input = "Input.txt";
+
+int Part1()
+{
+    return File.ReadAllLines(input).Sum(x =>
+        Range.ParseTwo(x.AsSpan()) switch
+        {
+            (Range, Range) r when r.Item1.Contains(r.Item2) => 1,
+            (Range, Range) r when r.Item2.Contains(r.Item1) => 1,
+            _ => 0,
+        }
+    );
+}
+
+int Part2()
+{
+    return File.ReadAllLines(input).Sum(x =>
+        Range.ParseTwo(x.AsSpan()) switch
+        {
+            (Range, Range) r when r.Item1.Overlaps(r.Item2) => 1,
+            _ => 0,
+        }
+    );
+}
+
+Console.WriteLine(Part1()); // 513
+Console.WriteLine(Part2()); // 878
+
+record Range(int Start, int End)
+{
+    public static Range Parse(ReadOnlySpan<char> text)
+    {
+        var index = text.IndexOf('-');
+        return new Range(int.Parse(text[..index]), int.Parse(text[(index + 1)..text.Length]));
+    }
+
+    public static (Range, Range) ParseTwo(ReadOnlySpan<char> text)
+    {
+        var index = text.IndexOf(',');
+        return (Range.Parse(text[..index]), Range.Parse(text[(index + 1)..text.Length]));
+    }
+
+    public bool Contains(int number) => Start <= number && End >= number;
+
+    public bool Contains(Range range) => Contains(range.Start) && Contains(range.End);
+
+    public bool Overlaps(Range range) => Contains(range.Start) || Contains(range.End) || range.Contains(Start) || range.Contains(End);
+}
