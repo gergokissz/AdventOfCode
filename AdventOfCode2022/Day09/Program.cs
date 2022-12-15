508f72766ab1a9ed351d0d8a67b3ba7f619555c0
gergokissz <16369083+gergokissz@users.noreply.github.com>
2022-12-15 15:51:21 +0100

    Add 2022 Day09 and Day10

---
+++ AdventOfCode2022/Day09/Program.cs
@@ -0,0 +1,98 @@
+﻿const string input = "Input.txt";
+
+int Part1()
+{
+    var rope = new Rope();
+
+    foreach (var line in File.ReadLines(input))
+    {
+        rope.Move(line);
+    }
+
+    return rope.TailPositions.Count;
+}
+
+int Part2()
+{
+    var rope = new Rope(10);
+
+    foreach (var line in File.ReadLines(input))
+    {
+        rope.Move(line);
+    }
+
+    return rope.TailPositions.Count;
+}
+
+Console.WriteLine(Part1()); // 5695
+Console.WriteLine(Part2()); // 2434
+
+public class Rope
+{
+    private readonly List<Position> _knots = new List<Position>();
+
+    public HashSet<Position> TailPositions { get; } = new HashSet<Position> { new Position() };
+
+    public Rope(int knotCount = 2)
+    {
+        for (int i = 0; i < knotCount; i++)
+        {
+            _knots.Add(new Position());
+        }
+    }
+
+    public Position Head => _knots[0];
+
+    public Position Tail => _knots[^1];
+
+    public void Move(string command)
+    {
+        var count = int.Parse(command.Substring(2));
+
+        for (int i = 0; i < count; i++)
+        {
+            switch (command[0])
+            {
+                case 'U':
+                    Head.Y += 1;
+                    break;
+                case 'D':
+                    Head.Y -= 1;
+                    break;
+                case 'L':
+                    Head.X -= 1;
+                    break;
+                case 'R':
+                    Head.X += 1;
+                    break;
+            }
+
+            MoveTail();
+        }
+    }
+
+    private void MoveTail()
+    {
+        for (int i = 1; i < _knots.Count; i++)
+        {
+            var head = _knots[i - 1];
+            var tail = _knots[i];
+
+            if (Math.Abs(head.X - tail.X) < 2 && Math.Abs(head.Y - tail.Y) < 2)
+            {
+                return;
+            }
+
+            tail.X += Math.Sign(head.X - tail.X);
+            tail.Y += Math.Sign(head.Y - tail.Y);
+        }
+
+        TailPositions.Add(Tail);
+    }
+}
+
+public record Position
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+}
---
+++ AdventOfCode2022/Day10/Program.cs
@@ -0,0 +1,68 @@
+﻿const string input = "Input.txt";
+
+int Part1()
+{
+    var cpu = new Cpu();
+
+    foreach (var line in File.ReadLines(input))
+    {
+        cpu.Execute(line);
+    }
+
+    return cpu.SignalStrength[19] + 
+           cpu.SignalStrength[59] +
+           cpu.SignalStrength[99] +
+           cpu.SignalStrength[139] +
+           cpu.SignalStrength[179] +
+           cpu.SignalStrength[219];
+}
+
+Console.WriteLine(Part1()); // 13680, PZGPKPEB
+
+class Cpu
+{
+    public int R { get; private set; } = 0;
+
+    public int X { get; private set; } = 1;
+
+    public int Cycle { get; private set; } = 1;
+
+    public List<int> SignalStrength { get; } = new List<int>();
+
+    public void Execute(string command)
+    {
+        if (command == "noop")
+        {
+            Tick();
+        }
+        else if (command.StartsWith("addx "))
+        {
+            Tick();
+            Tick();
+            X += int.Parse(command.Substring(5));
+        }
+    }
+
+    private void Tick()
+    {
+        SignalStrength.Add(X * Cycle);
+
+        var pixel = (Cycle % 40) - 1;
+
+        if (pixel == X - 1 || pixel == X || pixel == X + 1)
+        {
+            Console.Write("#");
+        }
+        else
+        { 
+            Console.Write(".");
+        }
+
+        if (pixel == -1)
+        {
+            Console.WriteLine();
+        }
+
+        Cycle++;
+    }
+}
